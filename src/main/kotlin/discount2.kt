39b45e05bcda5b93f0f515ba98757c761568b65b
Лесников Дмитрий <lesnikovborsette@gmail.com>
2024-07-08 11:38:20 +0300

    cange2

--- src/main/kotlin/discount2.kt
+++ src/main/kotlin/discount2.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    val result = calculateDiscount(0, 50_000, "Мир")
+    val result = calculateDiscount("Мир", 0, 50_000)
     if (result >= 0) println(result) else println("Лимит превышен, операция заблокирована")
     resetCounters()
 }
@@ -9,25 +9,27 @@ val dayLimit = 150_000
 val monthLimit = 600_000
 var dailyTransfer = 0
 
-fun calculateDiscount(transferPerMonth: Int, presentTransfer: Int, level: String): Int {
+fun calculateDiscount(level: String, transferPerMonth: Int, presentTransfer: Int): Int {
     dailyTransfer += presentTransfer
     val monthlyTransfer = transferPerMonth + dailyTransfer
     val discount = if (dailyTransfer <= dayLimit || monthlyTransfer <= monthLimit) {
         when (level) {
             "Mastercard" -> {
-                if (transferPerMonth > 75000) {
+                if (transferPerMonth + dailyTransfer - presentTransfer > 75000) {
                     (0.006 * presentTransfer + 20).toInt()
-                } else if (monthlyTransfer <= 75000) {
+                } else if (transferPerMonth + dailyTransfer <= 75000) {
                     0
-                } else if (monthlyTransfer - presentTransfer <= 75000) {
-                    ((monthlyTransfer - 75000) * 0.006 + 20).toInt()
-                } else (0.006 * presentTransfer + 20).toInt()
+                } else {
+                    ((transferPerMonth + dailyTransfer - 75000) * 0.006 + 20).toInt()
+                }
             }
+
             "Visa" -> {
                 if (0.0075 * presentTransfer < 35) {
                     35
                 } else (0.0075 * presentTransfer).toInt()
             }
+
             else -> {
                 0
             }
